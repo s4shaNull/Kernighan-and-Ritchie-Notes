a80609ac3671647fe6df667d55538c2047ec354d
s4shaNull <nguyendinhcuong0512@gmail.com>
2022-12-21 23:41:36 +0700

    Optimize htoi.c

--- Chap 2/4.The Type Conversions <ctype.h> Library/htoi.c
+++ Chap 2/4.The Type Conversions <ctype.h> Library/htoi.c
@@ -1,7 +1,7 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 
 unsigned long htoi(char s[]);
@@ -15,15 +15,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    char *s = malloc((strlen(argv[1]) + 1) * sizeof(char));
-
     /* You have to + 1 for the null terminator. The sizeof(char) could be
     omitted since C guarantee the size of a character is one byte in all platform*/
-
+    char *s = malloc((strlen(argv[1]) + 1) * sizeof(char));
     strcpy(s, argv[1]);
     
 
-    if (s[0] != '0' || (s[1] != 'x' && s[1] != 'X'))
+    if (s[0] != '0' || tolower(s[1] != 'x'))
     {
         printf("Please start your hexademical number with \'0x\' or \'0X\'\n");
         return 1;
@@ -33,50 +31,22 @@ int main(int argc, char **argv)
     return 0;
 }
 
-unsigned long power(int base, int exponent)
-{
-    if (exponent == 0)
-    {
-        return 1;
-    }
-
-    int i;
-    unsigned long res = base;
-
-    for (i = 1; i < exponent; i++)
-    {
-        res *= base;
-    }
-    
-    return res;
-}
-
 unsigned long htoi (char s[])
 {
-    int i, j, n;
+    int j;
+    char c;
     unsigned long res;
-    n = -1;
     res = 0;
-
-    for (i = 2; isxdigit(s[i]); i++)
-    {
-        n++;
-    }
 
     for (j = 2; isxdigit(s[j]); j++)
     {
         if (isdigit(s[j])) {
-            res = power(16,n--)*(s[j] - '0') + res;
+            res = res*16 + (s[j] - '0');
         }
-        else if (s[j] >= 'a' && s[j] <= 'f')
+        else if ((c = tolower(s[j])) >= 'a' && c <= 'f')
         {
-            res = power(16,n--)*(s[j] - 'a' + 10) + res;
-        }
-        else if (s[j] >= 'A' && s[j] <= 'F')
-        {
-            res = power(16,n--)*(s[j] - 'A' + 10) + res;
+            res = res*16 + c - 'a' + 10;
         }
     }
-
     return res;
 }
